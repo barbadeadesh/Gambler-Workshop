397d52f4ddc0c32771832c9ed12cf207c10c0bd1
barbadeadesh <barbadeadesh2020@email.com>
2022-03-07 15:49:15 +0530

    Game Start

---
+++ Uc1.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.wc;
+
+public class Uc1 {
+
+	public static final int STAKE = 100;
+	 public static final int BET = 1;
+	 
+	public static void main(String[] args) {
+		
+		System.out.println("Welcome to the game");
+		
+
+	}
+
+}
